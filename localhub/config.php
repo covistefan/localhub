d941ee2c0798d2d39ed63e4508ff41236906a15a
Stefan Händler <covistefan@users.noreply.github.com>
2017-06-29 13:46:29 +0200

    Create config.php

---
+++ localhub/config.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * author s.haendler@covi.de
+ * version 1.0
+ * lastchange 2017-06-29
+*/
+
+define('LOCALHUB', 'localhub');
+define('VERSIONS', 'versions.log');
+define('BASEVERSION', '1.0');
+define('SLUG', 'slug');
+define('LOCALDATE', 'd.m.Y, H:i');
+
+?>
